998595354db99a4c8a652514fd4fcd164010370f
Atharva Ahire <atharvahire2006@gmail.com>
2026-01-21 12:30:04 +0530

    Using JWT for authentication and connected to the database

--- JWT-USE/controllers/authController.js
+++ JWT-USE/controllers/authController.js
@@ -1,79 +1,79 @@
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
-// const Users = require("../database/Users");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const Users = require("../database/Users");
 
-// exports.register = async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
+exports.register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-//     if (!username || !password) {
-//       return res
-//         .status(400)
-//         .json({ message: "Username and password required" });
-//     }
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username and password required" });
+    }
 
-//     const existingUser = await Users.findOne({ where: { username } });
-//     if (existingUser) {
-//       return res.status(409).json({ message: "User already exists" });
-//     }
+    const existingUser = await Users.findOne({ where: { username } });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
 
-//     const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-//     const newUser = await Users.create({
-//       username,
-//       password: hashedPassword,
-//     });
+    const newUser = await Users.create({
+      username,
+      password: hashedPassword,
+    });
 
-//     res.status(201).json({
-//       message: "User registered successfully",
-//       user: {
-//         id: newUser.id,
-//         username: newUser.username,
-//       },
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: newUser.id,
+        username: newUser.username,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
-// exports.login = async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-//     const user = await Users.findOne({ where: { username } });
-//     if (!user) {
-//       return res.status(401).json({ message: "Invalid credentials" });
-//     }
+    const user = await Users.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
 
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       return res.status(401).json({ message: "Invalid credentials" });
-//     }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
 
-//     const token = jwt.sign(
-//       { id: user.id, username: user.username },
-//       process.env.JWT_SECRET,
-//       { expiresIn: "1h" },
-//     );
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
 
-//     res.cookie("token", token, {
-//       httpOnly: true,
-//       secure: false,
-//       sameSite: "lax",
-//       maxAge: 60 * 60 * 1000,
-//     });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 60 * 60 * 1000,
+    });
 
-//     res.json({ message: "Login successful" });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// };
+    res.json({ message: "Login successful" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
-// exports.dashboard = (req, res) => {
-//   res.json({
-//     message: "Protected data",
-//     user: req.user,
-//   });
-// };
+exports.dashboard = (req, res) => {
+  res.json({
+    message: "Protected data",
+    user: req.user,
+  });
+};
