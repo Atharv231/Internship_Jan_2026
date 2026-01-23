664e9755980d85f599336281cff005cdb56ef8c1
Atharva Ahire <atharvahire2006@gmail.com>
2026-01-23 12:43:28 +0530

    Using JWT for authentication and connected to the database

--- JWT-USE/routes/authRoutes.js
+++ JWT-USE/routes/authRoutes.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { login, register, dashboard } = require("../controllers/authController");
-const { refresh } = require("../controllers/authrefresh");
 
 const authMiddleware = require("../middleware/authMiddleware");
 
 // public
 router.post("/login", login);
 router.post("/register", register);
-router.post("/re", refresh);
 
 // protected
 router.get("/dashboard", authMiddleware, dashboard);
